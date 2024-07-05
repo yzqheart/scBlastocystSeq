6c1f5724f30f6aeeb615e0c55a625643f7d0da58
yzqheart <yzqheart@foxmail.com>
2024-07-05 14:13:22 +0800

    Create 04.hmmcopy.R

---
+++ 04.hmmcopy.R
@@ -0,0 +1,20 @@
+args<-commandArgs(TRUE)
+suppressMessages(library(HMMcopy))
+
+sample.name <- args[1] # sample name
+rfile <- args[2] # wig file produced by 03.readCounter.sh
+
+gfile <- "/mnt/data/yanzhiqiang/138/software/HMMcopy/hg38/hg38.gc.1000000.wig" # gc file produced by hmmcopy toolkit
+mfile <- "/mnt/data/yanzhiqiang/138/software/HMMcopy/hg38/hg38.map.1000000.bw" # map file produced by hmmcopy toolkit
+
+normal_reads <- wigsToRangedData(rfile, gfile, mfile)
+#normal_reads[100:110, ]
+
+normal_copy <- correctReadcount(normal_reads)
+#normal_copy[100:110, ]
+
+tumour_segments <- HMMsegment(normal_copy)
+#tumour_segments[10:110, ]
+
+write.csv(normal_copy,file = paste0(sample.name,".hmmcopy.win1000000.corrected.csv"),row.names = F,quote = F)
+write.csv(tumour_segments$segs,file = paste0(sample.name,".hmmcopy.win1000000.segment.csv"),row.names = F,quote = F)
