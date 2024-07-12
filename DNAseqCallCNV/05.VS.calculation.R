48dab3298575b098e9c4af2638103882538d2c70
yzqheart <yzqheart@foxmail.com>
2024-07-12 16:21:21 +0800

    Update 05.VS.calculation.R

--- DNAseqCallCNV/05.VS.calculation.R
+++ DNAseqCallCNV/05.VS.calculation.R
@@ -1,10 +1,10 @@
 rm(list = ls())
-sammples <- read.table("../samples.txt",header = F,stringsAsFactors = F)
+sammples <- read.table("samples.txt",header = F,stringsAsFactors = F)
 # samples.txt records the name of each cell
 
 sd.out <- c()
 for(s in sammples$V1){
-  df <- read.csv(paste0("win1M.hmmcopy/",s,".hmmcopy.win1000000.corrected.csv"),header = T,stringsAsFactors = F) # this is the corrected reads file produced by 04.hmmcopy.R
+  df <- read.csv(paste0(s,".hmmcopy.win1000000.corrected.csv"),header = T,stringsAsFactors = F) # this is the corrected reads file produced by 04.hmmcopy.R
   df <- df[!is.na(df$copy),]
   aggdata <- aggregate(df[,11],by=list(V1=df$chr),FUN=sd)
   sd.now <- round(median(aggdata$x),4)
